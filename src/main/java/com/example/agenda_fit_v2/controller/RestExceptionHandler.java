cc3b08f03570ed7e825d1e6e43fd849f9ba1d465
leandro.ferreira <leandroferreiramine@gmail.com>
2025-03-27 09:51:51 -0300

    exception argument type mismatch

--- src/main/java/com/example/agenda_fit_v2/controller/RestExceptionHandler.java
+++ src/main/java/com/example/agenda_fit_v2/controller/RestExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
@@ -37,6 +38,17 @@ public class RestExceptionHandler {
     public ProblemDetail handleRuntimeException(RuntimeException e) {
         log.error(e.getMessage(), e);
         return ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ProblemDetail handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e){
+        var errors = e.getMessage();
+        var pd = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+
+        pd.setTitle("Your parameters type are invalid");
+        pd.setProperty("InvalidParam", errors);
+
+        return pd;
     }
 
     private record InvalidParam(String name, String reason){}
